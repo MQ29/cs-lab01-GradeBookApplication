9be0fa93e774dd3f091bd0fa3c90705e2cb210e8
MQ29 <matiax2829@gmail.com>
2023-03-31 13:55:47 +0200

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,21 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+            if (averageGrade >= 80)
+                return 'A';
+            if (averageGrade < 80 && averageGrade >= 60)
+                return 'B';
+            if (averageGrade < 60 && averageGrade >= 40)
+                return 'C';
+            if (averageGrade < 40 && averageGrade >= 20)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
